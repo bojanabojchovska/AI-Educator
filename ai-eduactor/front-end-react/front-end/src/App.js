c7b1ddbe4f8d3b4e7155e6c8c9cc5afd35b6d604
mihaela4 <mihaela.nikolovska@students.finki.ukim.mk>
2025-04-27 01:43:46 +0200

    Added FlashcardsPage

--- ai-eduactor/front-end-react/front-end/src/App.js
+++ ai-eduactor/front-end-react/front-end/src/App.js
@@ -7,6 +7,7 @@ import HomePage from './components/HomePage';  // Don't forget to import the Hom
 import SemesterPage from "./components/SemesterPage";
 import CourseReviews from "./components/CourseReviews";
 import CourseReviewPage from './components/CourseReviewPage';
+import FlashcardsPage from "./components/FlashcardsPage";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import SubjectRecommendation from "./components/SubjectRecommendation";
 
@@ -24,6 +25,8 @@ function App() {
                     <Route path="/subject-recommendation" element={<SubjectRecommendation />} />
                     <Route path="/course-reviews" element={<CourseReviews />} />
                     <Route path="/course/:courseId/reviews" element={<CourseReviewPage />} />
+                    <Route path="/flash-cards" element={<FlashcardsPage />} />
+
                 </Routes>
             </div>
         </Router>

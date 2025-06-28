7a9f7ac049ba4b66a8dd3b7474bfabc633a1af6b
Bojana <bojana.bojchovska1@gmail.com>
2025-06-28 17:37:40 +0200

    changed folders, added dockerfiles for each services, added docker-compose.yml file

--- ai-eduactor/spring-boot-user-authentication-main/src/main/resources/db/migration/V5__on_delete_attachment_cascade_flashcards.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE flashcards
-DROP CONSTRAINT IF EXISTS fkl0pf9eo3w314s6yw3ev92qrti;
-
-ALTER TABLE flashcards
-    ADD CONSTRAINT fk_flashcards_attachment
-        FOREIGN KEY (attachment_id)
-            REFERENCES user_course_attachments(id)
-            ON DELETE CASCADE;

2dc9f04030be83ace393849d8bcc54685a708549
Bojana <bojana.bojchovska1@gmail.com>
2025-06-30 23:06:50 +0200

    changed ai service task to conversational

--- ai-eduactor/ai/models.py
+++ ai-eduactor/ai/models.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv(), override=True)
 
-def get_llm_model(repo_id="microsoft/Phi-3-mini-4k-instruct", task="text-generation", max_new_tokens=512, repetition_penalty=1.03, temperature=0.8):
+def get_llm_model(repo_id="microsoft/Phi-3-mini-4k-instruct", task="conversational", max_new_tokens=512, repetition_penalty=1.03, temperature=0.8):
     llm = HuggingFaceEndpoint(
         timeout=600,
         repo_id=repo_id,
